d5e95ca6ba9f9b051ec656a7872635ce1390cf85
Andreas Jensen <aj@energisk.dk>
2018-07-11 12:58:40 +0200

    Extract experian controller

---
+++ src/main/kotlin/eu/energisk/experian/service/controller/ExperianController.kt
@@ -0,0 +1,26 @@
+package eu.energisk.experian.service.controller
+
+import dk.rki.webservices.firma.Firma
+import dk.rki.webservices.firma.FirmaRegistreringData
+import dk.rki.webservices.person.Person
+import dk.rki.webservices.person.PersonAdresseData
+import eu.energisk.experian.service.configuration.ExperianClientConfiguration
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ExperianController(private val experianClientConfiguration: ExperianClientConfiguration) {
+    @GetMapping("/check")
+    fun getCheckPerson(@RequestParam cpr: String): PersonAdresseData? {
+        val client = Person().personSoap
+        val username = experianClientConfiguration.username
+        return client.soegPersonCpr(username, experianClientConfiguration.password, cpr)
+    }
+
+    @GetMapping("/check-company")
+    fun getCheckCompany(@RequestParam cvr: String): FirmaRegistreringData? {
+        val client = Firma().firmaSoap
+        return client.soegFirmaRegistreringCvr(experianClientConfiguration.username, experianClientConfiguration.password, cvr)
+    }
+}
