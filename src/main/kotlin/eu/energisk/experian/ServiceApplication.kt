122c82a4912068d4adc40e0cb92de0eb65759bc2
Andreas Jensen <aj@energisk.dk>
2018-07-13 13:53:45 +0200

    Wrap Person and Firma soap clients in a service class

--- src/main/kotlin/eu/energisk/experian/ServiceApplication.kt
+++ src/main/kotlin/eu/energisk/experian/ServiceApplication.kt
@@ -1,10 +1,35 @@
 package eu.energisk.experian
 
+import dk.rki.webservices.firma.Firma
+import dk.rki.webservices.firma.FirmaRegistreringData
+import dk.rki.webservices.firma.FirmaSoap
+import dk.rki.webservices.person.Person
+import dk.rki.webservices.person.PersonRegistreringData
+import dk.rki.webservices.person.PersonSoap
+import eu.energisk.experian.configuration.ExperianClientConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Service
 
 @SpringBootApplication
-class ServiceApplication
+class ServiceApplication {
+    @Bean
+    fun personClient(): PersonSoap {
+        return Person().personSoap
+    }
+
+    @Bean
+    fun firmaClient(): FirmaSoap {
+        return Firma().firmaSoap
+    }
+}
+
+@Service
+class ExperianClient(private val configuration: ExperianClientConfiguration, val person: PersonSoap, val firma: FirmaSoap) {
+    fun searchCreditHistoryByCpr(cpr: String): PersonRegistreringData = person.soegPersonRegistreringCpr(configuration.username, configuration.password, cpr)
+    fun searchCreditHistoryByCvr(cvr: String): FirmaRegistreringData = firma.soegFirmaRegistreringCvr(configuration.username, configuration.password, cvr)
+}
 
 fun main(args: Array<String>) {
     runApplication<ServiceApplication>(*args)
