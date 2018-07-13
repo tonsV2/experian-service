74cfdd8c54260fbfb722658568eea7626e50a0f9
Andreas Jensen <aj@energisk.dk>
2018-07-13 14:30:05 +0200

    Create interface for ExperianClient

--- src/main/kotlin/eu/energisk/experian/service/ExperianClient.kt
+++ src/main/kotlin/eu/energisk/experian/service/ExperianClient.kt
@@ -1,14 +1,9 @@
 package eu.energisk.experian.service
 
 import dk.rki.webservices.firma.FirmaRegistreringData
-import dk.rki.webservices.firma.FirmaSoap
 import dk.rki.webservices.person.PersonRegistreringData
-import dk.rki.webservices.person.PersonSoap
-import eu.energisk.experian.configuration.ExperianClientConfiguration
-import org.springframework.stereotype.Service
 
-@Service
-class ExperianClient(private val configuration: ExperianClientConfiguration, val person: PersonSoap, val firma: FirmaSoap) {
-    fun searchCreditHistoryByCpr(cpr: String): PersonRegistreringData = person.soegPersonRegistreringCpr(configuration.username, configuration.password, cpr)
-    fun searchCreditHistoryByCvr(cvr: String): FirmaRegistreringData = firma.soegFirmaRegistreringCvr(configuration.username, configuration.password, cvr)
+interface ExperianClient {
+    fun searchCreditHistoryByCpr(cpr: String): PersonRegistreringData
+    fun searchCreditHistoryByCvr(cvr: String): FirmaRegistreringData
 }
